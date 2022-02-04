e76354ddb0457052f7a66d02916afe57fe48eaf1
Ivan <IvanIsakov568@yandex.ru>
2022-02-04 16:56:05 +0300

    2ая задача выполнена

--- main.py
+++ main.py
@@ -4,32 +4,77 @@ import sys
 import pygame
 import requests
 
-ll = input('Введите координаты места (через запятую без пробела): ')
-spn = input('Введите масштаб (через запятую без пробела): ')
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l=map"
-response = requests.get(map_request)
+start_ll = list(map(float, input('Введите координаты места (через запятую без пробела): ').split(',')))
+start_spn = list(map(float, input('Введите масштаб (через запятую без пробела): ').split(',')))
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+def terminate(map_file):
+    pygame.quit()
 
-# Инициализируем pygame
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+    # Удаляем за собой файл с изображением.
+    os.remove(map_file)
+    sys.exit()
 
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+
+def program(start_ll, start_spn):
+    ll = start_ll
+    spn = start_spn
+
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={str(ll[0])}," \
+                  f"{str(ll[1])}&spn={str(spn[0])},{str(spn[1])}&l=map"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate(map_file)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_PAGEUP:
+                    spn[0] /= 2 # Уменьшаю масштаб в 2 раза и корректирую размер
+                    if spn[0] < 0.001:
+                        spn[0] = 0.001
+
+                    spn[1] /= 2
+                    if spn[1] < 0.001:
+                        spn[1] = 0.001
+                elif event.key == pygame.K_PAGEDOWN:
+                    spn[0] *= 2 # Уменьшаю масштаб в 2 раза и корректирую размер
+                    if spn[0] > 90:
+                        spn[0] = 90
+
+                    spn[1] *= 2
+                    if spn[1] > 90:
+                        spn[1] = 90
+
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={str(ll[0])}," \
+                      f"{str(ll[1])}&spn={str(spn[0])},{str(spn[1])}&l=map"
+        response = requests.get(map_request)
+
+        # Запишем полученное изображение в файл.
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+
+        # Рисуем картинку, загружаемую из только что созданного файла.
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        # Переключаем экран и ждем закрытия окна.
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    program(start_ll, start_spn)
